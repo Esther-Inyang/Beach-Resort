cfa65c9e9f08acdad7c855c5808b5ad8f2447566
Esther Inyang <estherinyang07@gmail.com>
2022-07-27 15:54:26 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Home  from './pages/Home'
+import Rooms  from './pages/Rooms'
+import SingleRoom  from './pages/SingleRoom'
+import Error  from './pages/Error'
 
 function App() {
   return (
     <div className="App">
       <h1>BEACH RESORT</h1>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
     </div>
   );
 }
